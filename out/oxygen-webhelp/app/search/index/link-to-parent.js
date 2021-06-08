d2712735adc9f0813b87675930126d4c33fcc4bc
joga11 <ad_rem@interia.eu>
2021-06-08 17:48:45 +0200

    initial

---
+++ out/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {3:-1,15:-1,6:15,18:7,7:9,19:7,0:15,17:7,1:9,14:11,11:-1,8:15,12:-1,4:12,5:3,13:11,9:-1,16:3,10:11,2:12};
+});
