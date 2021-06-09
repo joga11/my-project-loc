5ae72c90c936986fb497642af5dafebbfbf524c8
joga11 <ad_rem@interia.eu>
2021-06-09 03:08:18 +0200

    single publication removed

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210609025902';
+                args = '20210609030521';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
